b289c62f18cf51457289d182e4027edd02035424
Laura Alves <lauraalves.contato@gmail.com>
2025-09-20 17:05:49 -0300

    Implement js WIP

---
+++ js/geral.js
@@ -0,0 +1,9 @@
+const chave = "linha";
+const teste = document.querySelector("#teste");
+let letra1 = document.querySelector("#letra1");
+let letra2 = document.querySelector("#letra2");
+let letra3 = document.querySelector("#letra3");
+let letra4 = document.querySelector("#letra4");
+let letra5 = document.querySelector("#letra5");
+let palavra = letra1 + letra2 + letra3 + letra4 + letra5;
+
