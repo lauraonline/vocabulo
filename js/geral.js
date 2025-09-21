cb9ff5fa3cdf93264b981ed3b4e19b3f760085a4
Laura Alves <lauraalves.contato@gmail.com>
2025-09-21 20:40:09 -0300

    Implement VICTORY if user guesses the right word

--- js/geral.js
+++ js/geral.js
@@ -2,11 +2,26 @@ const chave = "linha";
 const teste = document.querySelector(".teste");
 const adivinhar = document.querySelector(".adivinhar");
 
+for (let i = 0; i < 5; i++) {
+    let seletor = ".letra" + (i+1) + ":not(disabled)";
+    document.querySelector(seletor).value = null;
+}
+
 adivinhar.addEventListener("click", function() {
     const palpite = [];
     for (let i = 0; i < 5; i++) {
         let seletor = ".letra" + (i+1) + ":not(disabled)";
         palpite[i] = document.querySelector(seletor).value;
     }
-   alert(palpite.join(""));
+    palpiteString = palpite.join("");
+    if (palpiteString == chave) {
+        for (let i = 0; i < 5; i++) {
+            let seletor = ".letra" + (i+1) + ":not(disabled)";
+            document.querySelector(seletor).classList.add("letra_verde");
+            document.querySelector(seletor).setAttribute("disabled", "true");
+        }
+        alert("Parabéns, você adivinhou a palavra!");
+    } else {
+
+    }
 });
