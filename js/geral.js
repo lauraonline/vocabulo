1f01578f7b852db6621f6c0916df5980685f3ed2
Laura Alves <lauraalves.contato@gmail.com>
2025-09-21 13:34:10 -0300

    add logic to GET the current guessed word

--- js/geral.js
+++ js/geral.js
@@ -1,9 +1,13 @@
 const chave = "linha";
-const teste = document.querySelector("#teste");
-let letra1 = document.querySelector("#letra1");
-let letra2 = document.querySelector("#letra2");
-let letra3 = document.querySelector("#letra3");
-let letra4 = document.querySelector("#letra4");
-let letra5 = document.querySelector("#letra5");
-let palavra = letra1 + letra2 + letra3 + letra4 + letra5;
+const teste = document.querySelector(".teste");
+const adivinhar = document.querySelector(".adivinhar");
 
+adivinhar.addEventListener("click", function() {
+    let letra1 = document.querySelector(".letra1:not(disabled)");
+    let letra2 = document.querySelector(".letra2:not(disabled)");
+    let letra3 = document.querySelector(".letra3:not(disabled)");
+    let letra4 = document.querySelector(".letra4:not(disabled)");
+    let letra5 = document.querySelector(".letra5:not(disabled)");
+    let palavra = letra1.value + letra2.value + letra3.value + letra4.value + letra5.value;
+    alert(palavra);
+});
