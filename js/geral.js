c8f799a666bacb03c84f1d44bf86cb1859645f38
Laura Alves <lauraalves.contato@gmail.com>
2025-09-21 19:46:23 -0300

    Implement logic to GET a guess from the active inputs

--- js/geral.js
+++ js/geral.js
@@ -3,11 +3,10 @@ const teste = document.querySelector(".teste");
 const adivinhar = document.querySelector(".adivinhar");
 
 adivinhar.addEventListener("click", function() {
-    let letra1 = document.querySelector(".letra1:not(disabled)");
-    let letra2 = document.querySelector(".letra2:not(disabled)");
-    let letra3 = document.querySelector(".letra3:not(disabled)");
-    let letra4 = document.querySelector(".letra4:not(disabled)");
-    let letra5 = document.querySelector(".letra5:not(disabled)");
-    let palavra = letra1.value + letra2.value + letra3.value + letra4.value + letra5.value;
-    alert(palavra);
+    const palpite = [];
+    for (let i = 0; i < 5; i++) {
+        let seletor = ".letra" + (i+1) + ":not(disabled)";
+        palpite[i] = document.querySelector(seletor).value;
+    }
+   alert(palpite.join(""));
 });
