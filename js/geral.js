fdd58847a101c9a02f4707c014128e2fcb287ca1
Laura Alves <lauraalves.contato@gmail.com>
2025-09-21 22:58:58 -0300

    Automatically focus next line after guessing

--- js/geral.js
+++ js/geral.js
@@ -1,10 +1,14 @@
 const chave = "linha";
 const teste = document.querySelector(".teste");
 const adivinhar = document.querySelector(".adivinhar");
+const quantidadeDeLetrasEm1Palavra = 5;
 
 const todasLetras = document.querySelectorAll(".letra");
 for (let i = 0; i < todasLetras.length; i++) {
     todasLetras[i].value = null;
+    if (i > quantidadeDeLetrasEm1Palavra - 1) {
+        todasLetras[i].setAttribute("disabled", "true");
+    }
 }
 
 adivinhar.addEventListener("click", function() {
@@ -14,7 +18,6 @@ adivinhar.addEventListener("click", function() {
         palpite[i] = document.querySelector(seletorLetraAtual).value;
     }
     palpiteString = palpite.join("");
-    alert(palpiteString);
     // TODO: verificar se o palpite feito pelo usuário está vazio ou não tem 5 letras
     // TODO: organizar o eventlistener pra chamar funções que chamam funções etc
     if (palpiteString == chave) {
@@ -29,14 +32,14 @@ adivinhar.addEventListener("click", function() {
         for (let i = 0; i < 5; i++) { // colorir cada letra adequadamente
             let seletorLetraAtual = ".letra" + (i+1) + ":not(:disabled)";
             if (chave.includes(palpite[i])) {
-                if (palpite[i] == chave[i]) {
+                if (palpite[i] == chave[i]) { // letra atual = verde
                     document.querySelector(seletorLetraAtual).classList.add("letra_verde");
                     document.querySelector(seletorLetraAtual).setAttribute("disabled", "true")
-                } else {
+                } else { // letra atual = amarela
                     document.querySelector(seletorLetraAtual).classList.add("letra_amarela");
                     document.querySelector(seletorLetraAtual).setAttribute("disabled", "true")
                 }
-            } else {
+            } else { // letra atual = cinza
                 document.querySelector(seletorLetraAtual).classList.add("letra_cinza");
                 document.querySelector(seletorLetraAtual).setAttribute("disabled", "true")
             }
@@ -45,5 +48,6 @@ adivinhar.addEventListener("click", function() {
             let seletorProxLetra = ".letra" + (i+1) + ":not(.letra_verde, .letra_amarela, .letra_cinza)";
             document.querySelector(seletorProxLetra).removeAttribute("disabled");
         }
+        document.querySelector(".letra1:not(:disabled)").focus(); // automaticamente posiciona o cursor na próxima linha
     }
 });
