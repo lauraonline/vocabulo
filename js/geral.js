4368439272d45983a57abe3fb8fa9c776c00c8b3
Laura Alves <lauraalves.contato@gmail.com>
2025-09-21 22:33:02 -0300

    Use a general letra class for all letters. This is useful in both css and js implementation

--- js/geral.js
+++ js/geral.js
@@ -2,29 +2,32 @@ const chave = "linha";
 const teste = document.querySelector(".teste");
 const adivinhar = document.querySelector(".adivinhar");
 
-for (let i = 0; i < 5; i++) {
-    let seletorLetraAtual = ".letra" + (i+1) + ":not(disabled)";
-    document.querySelector(seletorLetraAtual).value = null;
+const todasLetras = document.querySelectorAll(".letra");
+for (let i = 0; i < todasLetras.length; i++) {
+    todasLetras[i].value = null;
 }
 
 adivinhar.addEventListener("click", function() {
     const palpite = [];
     for (let i = 0; i < 5; i++) { // recolher todas as letras e preencher o vetor palpite
-        let seletorLetraAtual = ".letra" + (i+1) + ":not(disabled)";
+        let seletorLetraAtual = ".letra" + (i+1) + ":not(:disabled, .letra_verde, .letra_amarela, .letra_cinza)";
         palpite[i] = document.querySelector(seletorLetraAtual).value;
     }
     palpiteString = palpite.join("");
+    alert(palpiteString);
     // TODO: verificar se o palpite feito pelo usuário está vazio ou não tem 5 letras
+    // TODO: organizar o eventlistener pra chamar funções que chamam funções etc
     if (palpiteString == chave) {
-        for (let i = 0; i < 5; i++) {
-            let seletorLetraAtual = ".letra" + (i+1) + ":not(disabled)";
+        for (let i = 0; i < 5; i++) { // procedimento de vitória
+            let seletorLetraAtual = ".letra" + (i+1) + ":not(:disabled)";
             document.querySelector(seletorLetraAtual).classList.add("letra_verde");
             document.querySelector(seletorLetraAtual).setAttribute("disabled", "true");
         }
         alert("Parabéns, você adivinhou a palavra!");
+        document.querySelector(".adivinhar").style.display = "none";
     } else {
         for (let i = 0; i < 5; i++) { // colorir cada letra adequadamente
-            let seletorLetraAtual = ".letra" + (i+1) + ":not(disabled)";
+            let seletorLetraAtual = ".letra" + (i+1) + ":not(:disabled)";
             if (chave.includes(palpite[i])) {
                 if (palpite[i] == chave[i]) {
                     document.querySelector(seletorLetraAtual).classList.add("letra_verde");
